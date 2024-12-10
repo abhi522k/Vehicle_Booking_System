b7b1ac09870ccb2aa401952609c4b14ff73e3d1e
Abhishek Surse <abhisheksurse5@gmail.com>
2024-12-10 23:15:43 +0530

    Third commit navbar completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+import { FaSearch, FaRegHeart } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 import styles from "./Header.module.css";
 
 const Header = () => {
@@ -23,8 +26,35 @@ const Header = () => {
           <a href="#">Contact</a>
           {/* <a href="#">Login/Sign Up</a> */}
         </nav>
-        <div className="search_bar">Search</div>
-        <div className={styles["actions"]}>Actions</div>
+        <div className={styles["search_bar"]}>
+          <span className={styles["search_icon"]}>
+            <FaSearch />
+          </span>
+          <input
+            className={styles["search_input"]}
+            placeholder="Search for vehicles, brands and more."
+          />
+        </div>
+        <div className={styles["action_bar"]}>
+          <div className={styles["action_container"]}>
+            <span className="action_icon">
+              <CgProfile size="23px" />
+            </span>
+            <span className="action_name">Profile</span>
+          </div>
+          <div className={styles["action_container"]}>
+            <span className="action_icon">
+              <FaRegHeart size="23px" />
+            </span>
+            <span className="action_name">Wishlist</span>
+          </div>
+          <div className={styles["action_container"]}>
+            <span className="action_icon">
+              <MdOutlineShoppingCartCheckout size="23px" />
+            </span>
+            <span className="action_name">Booking</span>
+          </div>
+        </div>
       </header>
     </>
   );
